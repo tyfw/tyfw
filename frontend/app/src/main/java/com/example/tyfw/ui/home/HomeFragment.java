f58cf14beebc198e93254e45277fa143d9771a2a
clee <charlee0315@gmail.com>
2022-07-04 12:09:13 -0400

    Added features for changing graph view using dropdown menu inside home page
    TODO:
    - Allow better graph views for each option
    - Incorporate into profile pages

--- frontend/app/src/main/java/com/example/tyfw/ui/home/HomeFragment.java
+++ frontend/app/src/main/java/com/example/tyfw/ui/home/HomeFragment.java
@@ -1,10 +1,15 @@
 package com.example.tyfw.ui.home;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,12 +25,18 @@ import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+    private static String timeOption = "";
+
     private LineChart lineChart;
+    private Spinner dropdown;
+    private TextView currVal;
+    private TextView currStats;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -39,9 +50,18 @@ public class HomeFragment extends Fragment {
     }
 
 
+    @SuppressLint("SetTextI18n")
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState){
-        setChart(view);
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
+        lineChart = view.findViewById(R.id.home_chart);
+        dropdown = view.findViewById(R.id.home_graph_options);
+        currVal = view.findViewById(R.id.currentvalue);
+        currStats = view.findViewById(R.id.currentstats);
+
+        setChart();
+        setTimeOptions();
+        currVal.setText("69");
+        currStats.setText("420% worse");
     }
 
 
@@ -51,10 +71,32 @@ public class HomeFragment extends Fragment {
         binding = null;
     }
 
-    // Followed this tutorial: https://www.youtube.com/watch?v=TNeE9DJoOMY&list=PLgCYzUzKIBE9Z0x8zVUunk-Flx8r_ioQF&index=6
-    private void setChart(View view){
-        lineChart = view.findViewById(R.id.home_chart);
+    // Followed this API guide: https://developer.android.com/guide/topics/ui/controls/spinner.html#Populate
+    private void setTimeOptions(){
+        dropdown.setOnItemSelectedListener(new TimeSelectListener());
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(),R.array.time_options, android.R.layout.simple_spinner_item);
 
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        dropdown.setAdapter(adapter);
+    }
+
+    private class TimeSelectListener implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            timeOption = parent.getItemAtPosition(position).toString();
+            HomeFragment.this.setChart();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    }
+
+    // Followed this tutorial: https://www.youtube.com/watch?v=TNeE9DJoOMY&list=PLgCYzUzKIBE9Z0x8zVUunk-Flx8r_ioQF&index=6
+    // TODO: add custom x-y-margins for graph for each option
+    private void setChart(){
         ArrayList<String> xAxis = new ArrayList<>(); // each index contains String of what that is
         ArrayList<Entry> yAxis = new ArrayList<>(); // each index contains data point
         // Assuming xAxis and yAxis are set:
@@ -75,7 +117,7 @@ public class HomeFragment extends Fragment {
         lineChart.setData(res);
 
         lineChart.setVisibleXRange(0,100);
-
+        lineChart.notifyDataSetChanged();
         lineChart.invalidate();
     }
 
@@ -83,13 +125,41 @@ public class HomeFragment extends Fragment {
     private void updateData(ArrayList<String> xAxis, ArrayList<Entry> yAxis){
         float startTime = 0;
         float endTime = 100;
-        float timeScale = (float) 0.1;
 
-        for (float t = startTime; t<endTime; t+=timeScale){
+        float timeScale = (float) 0.01;
+
+        setCustom(timeOption.equals("Custom"));
+
+        switch (timeOption) {
+            case "Daily":
+                timeScale = (float) 0.1;
+                break;
+            case "Weekly":
+                timeScale = (float) 1.0;
+                break;
+            case "Monthly":
+                timeScale = (float) 10.0;
+                break;
+            case "Yearly":
+                timeScale = (float) 50.0;
+                break;
+            case "":
+                timeScale = (float) 0.05;
+                break;
+        }
+
+        for (float t = startTime; t < endTime; t += timeScale) {
             Entry val = new Entry(t, Float.parseFloat(String.valueOf(Math.sin(t))));
             yAxis.add(val);
             xAxis.add(String.valueOf(t));
         }
+    }
+
+    private void setCustom(boolean val){
+        lineChart.setTouchEnabled(val);
+        lineChart.setDragEnabled(val);
+        lineChart.setPinchZoom(val);
+        lineChart.setScaleEnabled(val);
     }
 
 
