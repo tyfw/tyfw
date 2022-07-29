417eddeae346e447a99a098ea093d11f1e81d99d
Jacob <jacobgnewman@icloud.com>
2022-07-28 17:39:36 -0700

    Get model loading functional

--- backend/src/ml.js
+++ backend/src/ml.js
@@ -1,7 +1,15 @@
 const tf = require('@tensorflow/tfjs');
+require("@tensorflow/tfjs-node");
 const fs = require('fs');
 
-const model = tf.layers.loadLayersModel('../data/model.json');
+const main = async () => {
 
-const predict = model.predict(tf.tensor([0.5, 0.5]));
-console.log(predict.dataSync());
+    const model = await tf.loadLayersModel('file://data/model.json');
+    let a = tf.tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.1, 0.2, 0.3, 0.4, 0.5, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]);
+    a = a.expandDims(1);
+    a = a.reshape([-1,24,1]);   
+
+    model.predict(a).print();
+    //console.log(predict.dataSync());
+}
+main().then(() => {}).catch((err) => {console.log(err)});
