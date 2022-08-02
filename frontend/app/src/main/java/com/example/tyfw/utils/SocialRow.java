443fd1b7c54052a99944224b44c9961f0fad1ef7
Zephaniah Ko <zephaniahko@gmail.com>
2022-08-01 17:17:24 -0700

    updated social fragment to try and have leaderboard like view, update getconvoid to init conversation if does not exist

---
+++ frontend/app/src/main/java/com/example/tyfw/utils/SocialRow.java
@@ -0,0 +1,15 @@
+package com.example.tyfw.utils;
+
+public class SocialRow {
+    private String name;
+    private String value;
+    private String walletAddress;
+
+    public String getName() { return name; }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
