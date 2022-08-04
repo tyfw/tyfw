cae027c5b4ae3813f1e11591262df9a450038391
Zephaniah Ko <zephaniahko@gmail.com>
2022-08-04 16:35:52 -0700

    codacy for user test

--- backend/test/user.test.js
+++ backend/test/user.test.js
@@ -1,13 +1,14 @@
 const user = require('../src/user.js')
-const {MongoClient} = require('mongodb')
+const {MongoClient} = require('mongodb');
 let connection;
 let db;
+
+const data = jest.mock('data')
 
 beforeAll(async () => {
     console.log(MongoClient)
     console.log(global.__MONGO_URI__)
     console.log(process.env.MONGO_URL)
-    // connection = await MongoClient.connect(global.__MONGO_URI__, {
     connection = await MongoClient.connect("mongodb://localhost:27017", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -16,7 +17,6 @@ beforeAll(async () => {
       await db.collection("users").deleteMany({});
       const teslaMockUser = new user.User("Tesla", "Nikola", "Tesla", "tesla@mail.com", ["0xa5cD18A9c0028853Cac10c778B03001e2c18aFF4"])
       const zephUser = new user.User("zeph", "Zeph", "Ko", "zeph@mail.com", ["0xDA9dfA130Df4dE4673b89022EE50ff26f6EA73Cf"])
-      const mikeUser = new user.User("mike", "Michael", "Scott", "mike@mail.com", ["0x0548F59fEE79f8832C299e01dCA5c76F034F558e"])
       db.collection("users").insertOne(teslaMockUser)
       db.collection("users").insertOne(zephUser)
       
